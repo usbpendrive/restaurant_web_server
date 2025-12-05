d97c973cde2f53c1e59df30099dddcb83ea8ddfc
usbpendrive <ferry.fernando@gmail.com>
2025-12-05 17:10:51 +0700

    Add 2 routes (menu, hours)

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ app.get('/',  async (req, res) => {
     return "Welcome to What's Fare is Fair!";
 });
 
-await app.listen({ port })
+app.get('/menu', async (req, res) => {
+    return "TODO: Menu Page";
+});
+
+app.get('/hours', async (req, res) => {
+    return "TODO: Hours Page";
+})
+
+await app.listen({ port });
 console.log(`Web server is listening at http://localhost:${port}!`);
