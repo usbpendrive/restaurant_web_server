b4561e91462edddc0b1dd5dce596dd9428eb7e14
usbpendrive <ferry.fernando@gmail.com>
2025-12-05 17:35:40 +0700

    static directory

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import ejs from 'ejs';
 import fastifyView from '@fastify/view';
+import fastifyStatic from '@fastify/static';
+import { join } from 'path';
 
 import operatingHours from "./data/operatingHours.js";
 import menuItems from "./data/menuItems.js";
@@ -10,11 +12,18 @@ import * as repl from "node:repl";
 const app = Fastify();
 const port = 3000;
 
+const publicPath = join(process.cwd(), 'public');
+
 app.register(fastifyView, {
     engine: {
         ejs: ejs,
     },
 });
+
+app.register(fastifyStatic, {
+    root: publicPath,
+    prefix: '/public/',
+})
 
 app.get("/", (req, reply) => {
     reply.view("views/index.ejs", { name: "What's Fare is Fair" });
